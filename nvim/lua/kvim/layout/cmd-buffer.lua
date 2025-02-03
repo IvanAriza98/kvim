fb7a0aa050fc961003ece061c444cfa2f392efb0
IvanAriza <ivan1998zgz@gmail.com>
2025-02-03 19:10:07 +0100

    Backup

--- nvim/lua/kvim/layout/cmd-buffer.lua
+++ nvim/lua/kvim/layout/cmd-buffer.lua
@@ -74,10 +74,20 @@ vim.keymap.set("n", "<F5>", function()
 	local buildPath = getConfigField(cfg_env, var.key.BUILD_PATH)
 	-- send_cmd(espIdfFullCleanCmd(buildPath))
 	-- send_cmd(espIdfSetTargetCmd(family, buildPath)) 
-	send_cmd(espIdfBuildCmd(buildPath))
+	-- send_cmd(espIdfBuildCmd(buildPath))
+        vim.cmd("botright split | term " ..espIdfBuildCmd(buildPath))
+
+        -- Configuraciones clave
+        vim.cmd([[
+            setlocal nobuflisted
+            setlocal bufhidden=hide
+            setlocal winfixheight   " Mantener altura fija
+            resize 12               " Altura inicial de 12 líneas
+        ]])
+
     end
-    renderer:render(body)
-    renderer:set_size({height = 30})
+    -- renderer:render(body)
+    -- renderer:set_size({height = 30})
 end, {noremap = true, desc = ""})
 
 vim.keymap.set("n", "<F6>", function()
@@ -85,12 +95,21 @@ vim.keymap.set("n", "<F6>", function()
     if cfg_env == var.env.ESP_IDF_C_CPP or cfg_env == var.env.ESP_IDF_MICRO then
 	local port = getConfigField(cfg_env, var.key.PORT)
 	local buildPath = getConfigField(cfg_env, var.key.BUILD_PATH)
-	send_cmd(espIdfFlashCmd(port, buildPath))
+	-- send_cmd(espIdfFlashCmd(port, buildPath))
+	vim.cmd("botright split | term " ..espIdfFlashCmd(port, buildPath))
     elseif cfg_env == var.env.PYTHON then
-	send_cmd(pythonExecuteCmd())
+	-- send_cmd(pythonExecuteCmd())
+	vim.cmd("botright split | term " ..pythonExecuteCmd())
     end
-    renderer:render(body)
-    renderer:set_size({height = 30})
+    -- Configuraciones clave
+    vim.cmd([[
+	setlocal nobuflisted
+	setlocal bufhidden=hide
+	setlocal winfixheight   " Mantener altura fija
+	resize 12               " Altura inicial de 12 líneas
+    ]])
+    -- renderer:render(body)
+    -- renderer:set_size({height = 30})
 end, {noremap = true, desc = ""})
 
 vim.keymap.set("n", "<F7>", function()
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 return {
     color_scheme = 'Tokyo Night Storm',
     enable_tab_bar = false,
-    window_padding = { left = 0, right = 0, top = 0, bottom = 0},
+    window_padding = { left = 20, right = 20, top = 20, bottom = 20},
 }
