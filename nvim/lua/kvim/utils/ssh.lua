f1383104f0af80f5b007fd2b9e7c5ff255fbc65a
IvanAriza <ivan1998zgz@gmail.com>
2025-02-23 18:38:24 +0100

    Added function to obtion file/directories from a ssh server to local machine

--- nvim/lua/kvim/utils/ssh.lua
+++ nvim/lua/kvim/utils/ssh.lua
@@ -13,7 +13,7 @@ function ssh_terminal(direction)
     term:toggle()  -- Toggle the terminal to show it
 end
 
-function transfer_file(file)
+function transfer_file(file, local_to_remote)
     local targetIp = getConfigField(vars.id.SSH, vars.key.TARGET_IP)
     local targetDir = getConfigField(vars.id.SSH, vars.key.TARGET_DIR)
     local targetUser = getConfigField(vars.id.SSH, vars.key.TARGET_USER)
@@ -22,7 +22,11 @@ function transfer_file(file)
 	print("Error: Missing parameters ...")
 	return
     end
-    os.execute(string.format("scp -P 10022 -r %s %s@%s:%s", file, targetUser, targetIp, targetDir))
+    if local_to_remote == true then
+	os.execute(string.format("scp -P 10022 -r %s %s@%s:%s", file, targetUser, targetIp, targetDir))
+    else
+	os.execute(string.format("scp -P 10022 -r %s@%s:%s %s", targetUser, targetIp, targetDir, vim.fn.expand("%:p:h")))
+    end
 end
 
 -- Ssh Connection 
@@ -41,12 +45,11 @@ end, {silent = true, noremap = true});
 -- Transfer actual file
 vim.keymap.set('n', "<C-t>af", function()
     local file = vim.api.nvim_buf_get_name(0)
-    print(file)
     if file == "" then
 	print("File not detected")
 	return
     end
-    transfer_file(file)
+    transfer_file(file, true)
 end, {silent = true, noremap = true});
 
 -- Mostrar menú de selección de archivos
@@ -58,10 +61,13 @@ vim.keymap.set('n', "<C-t>f", function()
         return
     end
     if vim.fn.filereadable(file_or_dir) == 1 or vim.fn.isdirectory(file_or_dir) == 1 then
-	transfer_file(file_or_dir)
+	transfer_file(file_or_dir, true)
     else
 	print("File or directory does not exist")
     end
 end, {silent = true, noremap = true})
 
-
+-- Función para transferir ficheros del servidor ssh a nuestra máquina local
+vim.keymap.set('n', "<C-g>f", function()
+    transfer_file(vim.fn.input("Enter gettting directory path: "), false)
+end, {silent = true, noremap = true})
