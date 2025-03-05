d3206989d2bcaf201dbb6699779b3044dc69c751
IvanAriza <ivan1998zgz@gmail.com>
2025-03-05 16:26:04 +0100

    Added multiple plugins and reoder in the repository

--- nvim/lua/kvim/config/custom-keys.lua
+++
@@ -1,4 +0,0 @@
--- General
-vim.api.nvim_set_keymap('n', '<C-s>', ':w!<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-q>', ':q!<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<ESC>', ':noh<CR>', { noremap = true })
