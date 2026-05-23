0abf6763e4933a2c8883448b503b9995baadab94
IvanAriza98 <ivan1998zgz@gmail.com>
2026-05-23 16:28:55 +0200

    feat(editor): add completion adn visual coding utilities

--- nvim/lua/kvim/plugins/editor.lua
+++ nvim/lua/kvim/plugins/editor.lua
@@ -32,4 +32,136 @@ return {
       })
     end,
   },
+  {
+    {
+        "kevinhwang91/nvim-ufo",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+
+        init = function()
+            vim.o.foldcolumn = "0"
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+            -- Evitamos que el nvim-ufo uso su foldtext propio
+            vim.o.foldtext = ""
+        end,
+
+        opts = {
+            provider_selector = function(_, filetype, _)
+                if filetype == "bash" or filetype == "sh" then
+                    return { "indent" }
+                end
+
+                return { "treesitter", "indent" }
+            end,
+        },
+
+        config = function(_, opts)
+            local ufo = require("ufo")
+
+            ufo.setup(opts)
+
+            vim.keymap.set("n", "zR", ufo.openAllFolds, {
+                desc = "Open all folds",
+            })
+
+            vim.keymap.set("n", "zM", ufo.closeAllFolds, {
+                desc = "Close all folds",
+            })
+
+            vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds, {
+                desc = "Open folds except selected kinds",
+            })
+
+            vim.keymap.set("n", "zm", ufo.closeFoldsWith, {
+                desc = "Close folds with level",
+            })
+
+            vim.keymap.set("n", "K", function()
+                local winid = ufo.peekFoldedLinesUnderCursor()
+                if not winid then
+                    vim.lsp.buf.hover()
+                end
+            end, {
+                desc = "Peek fold or LSP hover",
+            })
+        end,
+    },
+    },
+    {
+    {
+        "shellRaining/hlchunk.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+
+        config = function()
+            require("hlchunk").setup({
+                chunk = {
+                    enable = true,
+                    notify = false,
+                    priority = 15,
+                },
+
+                indent = {
+                    enable = true,
+                    notify = false,
+                    priority = 10,
+                },
+
+                line_num = {
+                    enable = false,
+                },
+
+                blank = {
+                    enable = false,
+                },
+            })
+        end,
+    },
+    },
+    {
+    {
+        "m4xshen/smartcolumn.nvim",
+        event = { "BufReadPost", "BufNewFile", "InsertEnter" },
+
+        opts = {
+            colorcolumn = "100",
+
+            disabled_filetypes = {
+                "help",
+                "text",
+                "markdown",
+                "dashboard",
+                "lazy",
+                "mason",
+                "neo-tree",
+                "NvimTree",
+                "terminal",
+                "toggleterm",
+                "Trouble",
+                "alpha",
+                "starter",
+                "checkhealth",
+                "lspinfo",
+            },
+
+            scope = "file",
+
+            custom_colorcolumn = {
+                lua = "100",
+                python = "100",
+                c = "100",
+                cpp = "100",
+                sh = "100",
+                bash = "100",
+                markdown = "120",
+                gitcommit = "72",
+            },
+
+            editorconfig = true,
+        },
+    },
+    }
 }
--- nvim/lua/kvim/plugins/init.lua
+++ nvim/lua/kvim/plugins/init.lua
@@ -2,6 +2,10 @@
 
 return {
   { import = "kvim.plugins.ui" },
+  { import = "kvim.plugins.lsp" },
+  { import = "kvim.plugins.dev" },
   { import = "kvim.plugins.editor" },
+  { import = "kvim.plugins.dashboard" },
   { import = "kvim.plugins.navigation" },
+  { import = "kvim.plugins.completion" },
 }
