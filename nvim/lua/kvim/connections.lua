3fd49669fe36fd8f75bbb998d9d6017afe11aff9
IvanAriza98 <ivan1998zgz@gmail.com>
2026-06-06 10:33:36 +0200

    feat(workspace): management connection from connections.lua. In the workspace file only request this file

--- nvim/lua/kvim/connections.lua
+++ nvim/lua/kvim/connections.lua
@@ -1,11 +1,4 @@
 return { {
-    baudrate = 115200,
-    command = "picocom",
-    device = "/dev/ttyACM0",
-    name = "ESP32-C6 RCP",
-    tags = { "embedded", "thread" },
-    type = "serial"
-  }, {
     host = "127.0.0.1",
     identity_file = "/home/KODVMV/.ssh/kvim_docker-test-1_ed25519",
     name = "docker-test-1",
